21d08c35b73c0131e49c2c2bf07dab260c524c8a
jheroje <javierho93@gmail.com>
2021-03-06 16:40:07 +0000

    modified

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteTitle: 'Javier Hernandez Ojeda', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Javier',
+  lastName: 'Hernandez Ojeda',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/jheroje',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/javier-hernandez-ojeda-1932abb8/',
+    }
+  ],
+  email: 'javierho93@gmail.com',
+  phone: '000-00000',
+  address: 'Las Palmas de Gran Canaria',
+};
