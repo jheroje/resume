62830998594f1c1f9aea24b4694fac721d353b4b
jheroje <javierho93@gmail.com>
2022-01-05 22:57:09 +0000

    small change

--- src/pages/index.js
+++ src/pages/index.js
@@ -149,7 +149,7 @@ const IndexPage = () => {
 
             <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
               <div className="resume-content">
-                <h3 className="mb-0">Full Stack Developer Intern</h3>
+                <h3 className="mb-0">Full Stack Developer</h3>
                 <div className="subheading mb-3">Edosoft Factory</div>
                 <p>Tech stack:</p>
                 <ul>
