f09f7ec304b63ffb050984ca2ed97df8a076ac35
jheroje <javierho93@hotmail.com>
2022-01-07 23:57:17 +0000

    fix ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
-import React from 'react';
-import Layout from './src/components/Layout';
-
-export const wrapPageElement = ({ element }) => {
-  return <Layout>{element}</Layout>;
-};
